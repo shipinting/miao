22bf3083f2f13b4a82d46d1631155ead44f42277
Iris <1147063667@qq.com>
2018-07-16 20:40:57 +0800

    shipinting-lodash.js

--- lodash/shipinting-lodash.js
+++ lodash/shipinting-lodash.js
@@ -1,9 +1,19 @@
 var shipinting = {
-  chunk: function f(array,size) {
+  chunk: function(array,size) {
     var res = []    
     for (var i = 0; i < array.length; i += size) {      
       res.push(array.slice(i,i + size))
     }
     return res
   },
+  compact: function(array) {
+    for (var i = 0; i < array.length; i++) {
+      if (!array[i]) {
+        array.splice(i,1)
+        i--
+      }
+    }
+    return array
+  },
+
 }
