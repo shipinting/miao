0b047e26e5fb0e8887d8f7b52f9d7f4316dab8dd
Iris <1147063667@qq.com>
2018-07-23 17:40:02 +0800

    add lodash

--- lodash/shipinting-lodash.js
+++ lodash/shipinting-lodash.js
@@ -221,7 +221,40 @@ var shipinting = {
    }
     return false
   }
-  
+  ,forEach: (ary, iteratee=_.identity) => {
+    var func = shipinting.iteratee(iteratee)
+    for (var i = 0; i < ary.length; i++) {
+      func(ary[i])
+    }
+  }
+  ,filter: (ary, predicate = shipinting.identity) => {
+    var func = shipinting.iteratee(predicate)
+    var res = []
+    ary.forEach(item => {
+      if (func(item)) res.push(item)
+    })
+    return res
+  }
+  ,map: (ary, iteratee=_.identity) => {
+    var func = shipinting.iteratee(iteratee)
+    var map = []
+    ary.forEach(item => {
+      map.push(func(item))
+    })
+    return map
+  }
+  ,reduce: (ary, iteratee=_.identity, initialValue) => {
+    var func = shipinting.iteratee(iteratee)
+    ary.forEach(item => {
+      initialValue = func(initialValue,item)
+    })
+    return initialValue
+  }
+  ,sumBy: (ary, iteratee=_.identity) => {
+    var func = shipinting.iteratee(iteratee)
+    return ary.reduce((result,item) => {
+      return func(result) + func(item)
+    })  
   }
 
 
