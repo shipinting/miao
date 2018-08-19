554458f2f8964c5f14c507aac58a2cbf5b0ef8c8
Iris <1147063667@qq.com>
2018-08-19 22:35:21 +0800

    add lodash

--- lodash/shipinting-lodash.js
+++ lodash/shipinting-lodash.js
@@ -30,24 +30,23 @@ var shipinting = {
     }
   return res    
   }
-  ,differenceBy: (array, ...values) => {
+  ,differenceBy: (array, values) => {
     var res = []
     var map = []
-    var iteratee = values[values.length - 1]
     if (typeof iteratee === 'function') {      
-      for (var i = 0; i < values.length - 1; i++) {
-        map[iteratee(values[i])] = 1
+      for (var i of values) {
+        map[iteratee(i)] = 1
       }
       for (var j of array) {
         if (!map[iteratee(j)]) res.push(j)
       }
     return res
     }else{
-      for (var k = 0; k < values.length - 1; k++) {
-        map[values[k]] = 1
-      }      
+      for (var i of values) {
+        map[i[iteratee]] = 1
+      }
       for (var j of array) {
-        if (!map[values[j]]) res.push(j)
+        if (!map[j[iteratee]]) res.push(j)
       }
     return res
     }
